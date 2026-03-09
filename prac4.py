58e15eff5e6b4ecb9da1b9cc4448248beb534be7
Mr-Nasty <97503871+Mr-Nasty@users.noreply.github.com>
2026-03-10 02:21:28 +0530

    Refactor lambda_handler for better logging and response
    Refactor lambda_handler to improve logging and response structure.

--- prac4.py
+++ prac4.py
@@ -5,21 +5,32 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    length = event['length']
-    width = event['width']
-    area = calculateArea(length, width)
 
-    data = {
+    # Get values from event
+    length = event["length"]
+    width = event["width"]
+
+    # Calculate area
+    area = calculate_area(length, width)
+
+    # Log information
+    logger.info("Length: %s, Width: %s", length, width)
+    logger.info("Area calculated: %s", area)
+
+    # Response
+    response = {
         "length": length,
-        "width": width
+        "width": width,
+        "area": area
     }
 
-    return json.dumps(data)
+    return response
 
-def calculateArea(length, width):
+
+def calculate_area(length, width):
     return length * width
 
 #{
-#  "length":23
-#  'width": 45
+#  "length": 23,
+#  "width": 45
 #}
