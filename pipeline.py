250e4c3fdcb39de48092f208142a98201c9e2664
Mr-Nasty <97503871+Mr-Nasty@users.noreply.github.com>
2026-03-10 08:10:06 +0530

    Add Jenkins pipeline test script

---
+++ pipeline.py
@@ -0,0 +1,12 @@
+print("Jenkins Pipeline Test Started")
+
+a = 10
+b = 5
+
+sum = a + b
+product = a * b
+
+print("Sum:", sum)
+print("Product:", product)
+
+print("Jenkins Pipeline Executed Successfully")
